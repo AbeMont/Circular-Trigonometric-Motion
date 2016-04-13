81faf847b17c0516f1fb7c6ba880e321699949cc
Abraham Montoya <abraham.montoya@gmail.com>
2016-04-13 00:45:15 -0500

    motion is working

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 var t = 0;
 
 function moveit() {
-    t += 0.05;
+    t += 0.05; //if tangent is 0, the rocket wont move
 
     var r = 20;
     var xcenter = 70;
     var ycenter = 70;
     var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
     var newTop = Math.floor(ycenter + (r * Math.sin(t)));
+
     $('#rocket').animate({
         top: newTop,
         left: newLeft,
-    }, 1, function() {
+    },
+    1,
+    function() {
         moveit();
     });
+
 };
 
 
@@ -23,7 +27,7 @@ $(document).ready(function() {
     	moveit();
     });
      $("#rocket").mouseleave(function () {
-    	moveit();
+       $(this).stop();
     });
 
 });
