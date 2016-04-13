27f6fa9e905272a8bb3a57553b43f97265f036e2
Abraham Montoya <abraham.montoya@gmail.com>
2016-04-12 21:21:50 -0500

    Rocket is Launching

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var t = 0;
+
+function moveit() {
+    t += 0.05;
+
+    var r = 20;
+    var xcenter = 70;
+    var ycenter = 70;
+    var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
+    var newTop = Math.floor(ycenter + (r * Math.sin(t)));
+    $('#rocket').animate({
+        top: newTop,
+        left: newLeft,
+    }, 1, function() {
+        moveit();
+    });
+};
+
+
+$(document).ready(function() {
+
+    $("#rocket").mouseenter(function () {
+    	moveit();
+    });
+     $("#rocket").mouseleave(function () {
+    	moveit();
+    });
+
+});
